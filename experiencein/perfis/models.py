43539d7083921f593fbc4e70a6fe0013c471299c
Your Name <kalita.rodriguesdf@gmail.com>
2022-10-04 17:04:13 -0300

    models

--- experiencein/perfis/models.py
+++ experiencein/perfis/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+class Perfil(models.Model):
 
-# Create your models here.
+    nome = models.CharField(max_length=255, null=False)
+    email = models.CharField(max_length=255, null=False)     
+    telefone = models.CharField(max_length=15, null=False)
+    nome_empresa = models.CharField(max_length=255, null=False)
+
+class Convite(models.Model):
+   solicitante = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_feitos')
+   convidado = models.ForeignKey(Perfil, on_delete=models.CASCADE, related_name='convites_recebidos')
+
+def convidar(self, perfil_convidado):
+        Convite(solicitante=self, convidado=perfil_convidado).save()
