5b75c130c060c8239c8b4919b1a73949f5cc310f
Your Name <kalita.rodriguesdf@gmail.com>
2022-10-04 16:51:13 -0300

    views.py

--- experiencein/perfis/views.py
+++ experiencein/perfis/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from perfis.models import Perfil
 
-# Create your views here.
+from django.shortcuts import redirect
+
+def convidar(request, perfil_id):
+    perfil_a_convidar = Perfil.objects.get(id=perfil_id)
+    perfil_logado = get_perfil_logado(request)
+    perfil_logado.convidar(perfil_a_convidar)
+
+    return redirect('index')
+
+def get_perfil_logado(request):
+    return Perfil.objects.get(id=1)
