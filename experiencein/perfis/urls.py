9c364099276aa47bfbc5cc1d023e3f9940319572
Your Name <kalita.rodriguesdf@gmail.com>
2022-10-04 16:44:57 -0300

    EdModificado os arquivos

---
+++ experiencein/perfis/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from perfis import views
+urlpatterns = [
+	path('', views.index, name='index'),
+	path('perfis/<int:perfil_id>', views.exibir, name='exibir'),
+	path('perfis/<int:perfil_id>/convidar', views.convidar, name='convidar')
+]
